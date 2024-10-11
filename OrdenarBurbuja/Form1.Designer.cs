49d7db1158fc830f1f822270c96417cc5991eb54
KICKFURY <guzmancito16032018dqtc@gmail.com>
2024-10-11 13:32:35 -0600

    Agregar archivos de proyecto.

---
+++ OrdenarBurbuja/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace OrdenarBurbuja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnOrdernar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lstNumeros = new System.Windows.Forms.ListBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(12, 39);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(87, 37);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnOrdernar
+            // 
+            this.btnOrdernar.Location = new System.Drawing.Point(12, 90);
+            this.btnOrdernar.Name = "btnOrdernar";
+            this.btnOrdernar.Size = new System.Drawing.Size(87, 37);
+            this.btnOrdernar.TabIndex = 0;
+            this.btnOrdernar.Text = "Ordenar";
+            this.btnOrdernar.UseVisualStyleBackColor = true;
+            this.btnOrdernar.Click += new System.EventHandler(this.btnOrdernar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(12, 141);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(87, 37);
+            this.btnSalir.TabIndex = 0;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lstNumeros
+            // 
+            this.lstNumeros.FormattingEnabled = true;
+            this.lstNumeros.Location = new System.Drawing.Point(132, 22);
+            this.lstNumeros.Name = "lstNumeros";
+            this.lstNumeros.Size = new System.Drawing.Size(146, 186);
+            this.lstNumeros.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 211);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(67, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(55, 22);
+            this.toolStripLabel1.Text = "Cantidad";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 233);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.lstNumeros);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnOrdernar);
+            this.Controls.Add(this.btnCargar);
+            this.Name = "Form1";
+            this.Text = "Burbuja";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnOrdernar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ListBox lstNumeros;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
---
+++ OrdenarBurbuja/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrdenarBurbuja
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int[] numeros;
+
+        public Form1()
+        {
+            InitializeComponent();
+            numeros = new int[1000];
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOrdernar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 1000; i++)
+            {
+                lstNumeros.Items.Add(random.Next(1000));
+            }
+
+            toolStrip1.Text = lstNumeros.Items.Count.ToString();
+        }
+    }
+}
